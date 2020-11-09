15fa4ab1724ec91c173abd7b54377aac2b9fb7f8
shubham1592 <pi.shubham1592@gmail.com>
2020-11-09 20:18:01 +0530

    Initial commit

---
+++ lib/src/app.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'screens/login_screen.dart';
+class App extends StatelessWidget{
+  Widget build(context){
+    return MaterialApp(
+      title: 'Log me in',
+      home: Scaffold(
+        body: LoginScreen(),
+      ),
+    );
+  }
+}
